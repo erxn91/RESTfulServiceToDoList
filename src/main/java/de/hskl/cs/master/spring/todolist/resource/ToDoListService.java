83bcd50a6f898edd2f66a60c2bc38fe953808845
erxn91 <eric.brunk91@googlemail.com>
2018-11-07 17:36:49 +0100

    ToDoListService init

--- src/main/java/de/hskl/cs/master/spring/todolist/resource/ToDoListService.java
+++ src/main/java/de/hskl/cs/master/spring/todolist/resource/ToDoListService.java
@@ -1,4 +1,26 @@
 package de.hskl.cs.master.spring.todolist.resource;
 
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class ToDoListService {
+
+    private List<User> _userList;
+
+    public ToDoListService() {
+        this._userList = new ArrayList<>();
+    }
+
+    public boolean addUser() {
+        User user = new User();
+        return this._userList.add(user);
+    }
+
+    public boolean addUser(String firstname, String lastname) {
+        User user = new User(firstname, lastname);
+        return this._userList.add(user);
+    }
 }
